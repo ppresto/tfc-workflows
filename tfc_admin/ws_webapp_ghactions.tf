b310c8f96c00c0eafe168e0de73ed69c9e859373
ppresto <ppresto@hashicorp.com>
2021-09-20 14:52:28 -0700

    2

--- tfc_admin/ws_webapp_ghactions.tf
+++ tfc_admin/ws_webapp_ghactions.tf
@@ -1,12 +1,3 @@
-data "tfe_team" "test" {
-  name         = "owners"
-  organization = var.organization
-}
-
-resource "tfe_team_token" "owner" {
-  team_id = data.tfe_team.test.id
-}
-
 module "webapp_ghactions_ws" {
     source = "../modules/workspace-mgr-api"
     agent_pool_id     = ""
